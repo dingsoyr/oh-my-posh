2e843b584369714512e6800382e9c58f39b9b5ef
Sture Dingsøyr <candalt@online.no>
2024-11-13 10:23:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-if [ "$(whoami)" != "root" ] ; then echo "Please run as root!"; exit; fi
-
 echo "Installing Oh My Posh"
 curl -s https://ohmyposh.dev/install.sh | bash -s -- -t /home/difi/.cache/oh-my-posh
 chown -R difi:difi .cache/oh-my-posh/
@@ -10,13 +8,14 @@ echo ""
 echo "Installing Nerd fonts"
 cd /home/difi
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Hack.zip
-mkdir /usr/local/share/fonts/Hack
-unzip Hack.zip -d /usr/local/share/fonts/Hack/
-apt install fontconfig
-fc-cache -fv
-rm -rf /home/difi/Hack.zip
+sudo mkdir /usr/local/share/fonts/Hack
+sudo unzip Hack.zip -d /usr/local/share/fonts/Hack/
+sudo apt install fontconfig
+sudo fc-cache -fv
+sudo rm -rf /home/difi/Hack.zip
 echo ""
 
 echo "Finished"
 echo "Add this line to .baschrc to change prompt"
+echo "export PATH=\$PATH:/home/difi/.local/bin"
 echo "eval \"\$(oh-my-posh init bash --config https://raw.githubusercontent.com/dingsoyr/oh-my-posh/main/sture.omp.json)\""
